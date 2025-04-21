7acef909a7086adeab4ad755bbeb955cc1841694
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-04-21 19:56:48 +0200

    records added

--- dcrm/website/admin.py
+++ dcrm/website/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Record  # Model wird importiert, muss mit makemigration und dann migrate in die DB geschoben werden
 
 # Register your models here.
+admin.site.register(Record)   # wird im Admin-Interface registriert und kann dort angezeigt werden
